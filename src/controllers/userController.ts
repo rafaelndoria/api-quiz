1ca450abfd33cd338c5d7112dc7cb0dae23ab487
dorianascimento <rafaelnd2001@gmail.com>
2023-03-05 19:22:13 -0300

    feat: created endpoint for login

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,13 +2,37 @@ import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import User from '../models/User';
 
-export const login = (req: Request, res: Response) => {
+export const login = async (req: Request, res: Response) => {
+    if(req.body.email && req.body.password) {
+        let { email, password } = req.body;
+        let hasUser = await User.findOne({
+            email
+        });
 
+        if(hasUser) {
+            let passwordCorrect = await bcrypt.compare(password, hasUser.password);
+
+            if(passwordCorrect) {
+                res.json({ login: true });
+            } else {
+                res.status(404);
+                res.json({ error: 'password is not correct' });
+            }
+
+        } else {
+            res.status(400);
+            res.json({ error: 'user does not exists' });
+        }
+
+    } else {
+        res.status(400);
+        res.json({ error: 'missing data' });
+    }
 }
 
 export const register = async (req: Request, res: Response) => {
     if(req.body.email && req.body.password && req.body.user) {
-        let {user, email, password} = req.body;
+        let { user, email, password } = req.body;
         let hasUser = await User.findOne({
             email
         });
