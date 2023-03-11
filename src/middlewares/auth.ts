1d0306a7c77bc58b98df6185f4a34c19c789def8
dorianascimento <rafaelnd2001@gmail.com>
2023-03-11 08:59:03 -0300

    fix: edited authentication to show which user is logged in jwt token and manipulate it in controllers

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,32 +4,30 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const Auth = {
-    private: (req: Request, res: Response, next: NextFunction) => {
-        let success = false;
+export interface AuthRequest extends Request {
+    userId?: string;
+    userEmail?: string;
+}
+  
+export function Auth(req: AuthRequest, res: Response, next: NextFunction) {
+    const authHeader = req.headers.authorization;
 
-        if(req.headers.authorization) {
+    if (!authHeader) {
+        return res.status(401).json({ error: 'not authorized' });
+    }
 
-            const [authType, token] = req.headers.authorization.split(' ');
-            if(authType === 'Bearer') {
-                try {
-                    const decoded = JWT.verify(
-                        token, 
-                        process.env.JWT_SECRET_KEY as string
-                    );
-                    success = true;
-                } catch(error) {
-
-                }
-            }
-
-        }
-
-        if(success) {
+    const [type, token] = authHeader.split(' ');
+    
+    if(type === 'Bearer') {
+        try {
+            const decodedToken = JWT.verify(token, process.env.JWT_SECRET_KEY as string) as { id: string, email: string };
+            req.userId = decodedToken.id;
+            req.userEmail = decodedToken.email;
             next();
-        } else {
-            res.status(403);
-            res.json({ error: 'not authorized' });
+        } catch (error) {
+            return res.status(401).json({ error: 'invalid token' });
         }
+    } else {
+        return res.status(401).json({ error: 'only bearer type' });
     }
 }
